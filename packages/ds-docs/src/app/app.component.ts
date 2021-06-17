55725c020b179091e390d479f56b16d8af6495c8
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2021-06-17 16:42:43 +0200

    Option list renders

--- packages/ds-docs/src/app/app.component.ts
+++ packages/ds-docs/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IOpOptionListOption } from '@openproject/angular';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ds-docs';
+
+  selectedOption = 'No option selected';
+
+  options: IOpOptionListOption<string>[] = [
+    {
+      value: 'With description',
+      title: 'With description',
+      disabled: false,
+      description: 'This option has a description',
+    },
+    {
+      value: 'Without description',
+      title: 'Without description',
+      disabled: false,
+      description: '',
+    },
+    {
+      value: 'Disabled with description',
+      title: 'Disabled with description',
+      disabled: true,
+      description: 'This is disabled and has a description',
+    },
+    {
+      value: 'Almost useless',
+      title: 'Almost useless',
+      disabled: true,
+      description: '',
+    },
+  ];
 }
