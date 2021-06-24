8a54983481f1f2479fe1129dcd47702a73c7186d
Benjamin Bädorf <hello@benjaminbaedorf.eu>
2021-06-24 17:21:34 +0200

    Add basic openproject theming

---
+++ .storybook/OpenProjectTheme.js
@@ -0,0 +1,9 @@
+import { create } from '@storybook/theming';
+
+export default create({
+  base: 'light',
+  brandTitle: 'OpenProject',
+  brandUrl: 'https://openproject.org',
+  brandImage: 'https://www.openproject.org/assets/images/openproject-logo-color-c9a6dfe3.svg',
+});
+
