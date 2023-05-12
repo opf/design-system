74535fc2458921b8a87eef4383b1ad859bb7d2c4
OpenProject CI <operations+openprojectci@openproject.com>
2023-05-12 11:52:31 +0000

    [Fri May 12 11:52:31 UTC 2023] Latest storybook from dev

---
+++ docs/sb-addons/storybook-11/manager-bundle.js
@@ -0,0 +1,4 @@
+try{
+var c=__STORYBOOKADDONS__,{addons:o,types:i,mockChannel:O}=__STORYBOOKADDONS__;var b=__STORYBOOKTHEMING__,{CacheProvider:S,ClassNames:u,Global:x,ThemeProvider:y,background:C,color:D,convert:N,create:a,createCache:R,createGlobal:k,createReset:B,css:G,darken:K,ensure:P,ignoreSsrWarning:Y,isPropValid:j,jsx:v,keyframes:I,lighten:A,styled:E,themes:H,typography:M,useTheme:w,withTheme:U}=__STORYBOOKTHEMING__;var s=a({base:"light",brandTitle:"OpenProject Design System",brandUrl:"/",brandImage:"/assets/logo_openproject_spot.png",brandTarget:"_self"});o.setConfig({theme:s});
+}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
+//# sourceMappingURL=manager-bundle.js.map
--- docs/sb-addons/storybook-plugin-iframe-src-10/register-bundle.mjs
+++
@@ -1,4 +0,0 @@
-try{
-var _=__STORYBOOKADDONS__,{addons:d,types:D,mockChannel:l}=__STORYBOOKADDONS__;var r="iframe";window&&window.parent&&d.register(r,()=>{let t=window.location.toString();document.body.addEventListener("click",()=>{let o=window.location.toString();t!==o&&(window.parent.postMessage(o,"*"),t=o)})});
-}catch(e){ console.error("[Storybook] One of your manager-entries failed: " + import.meta.url, e); }
-//# sourceMappingURL=register-bundle.mjs.map
